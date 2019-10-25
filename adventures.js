07b14467b78083d10567e917bb03861e2bcca236
Searl104 <56383835+Searl104@users.noreply.github.com>
2019-10-24 19:28:13 -0700

    fix

--- adventures.js
+++ adventures.js
@@ -4,13 +4,18 @@ class adventure {
         this.body = body;
     }
 }
+
+var adventureXML;
+
 var adventures = [
     new adventure("Adventure 1: Caverns and Kobolds",
+
     `Caelynn, Lokt, Ordrin, Emerald, Verna, Galant, and Adara traveled to Renston to investigate a kobold problem. There they raided the lizard's cave, eliminating many of the creatures.
     <p>In the depths they fought a shaman like kobold who commanded a pair of ogres. With the foes defeated, the party took their pick of a treasure hoard. They left the egg clutch intact with a warning and departed for home.
     <p>- Recorded by Ordrin`),
 
     new adventure("Adventure 2 : After Andrews",
+
     `Ordrin, Lokt, Thalion and Verna team up to bring in Andrews and his gang. After a close encounter with a waterfall, Verna lets Ordrin take the role of navigator.
     <p>They find the coast and make camp, but that night a powerful storm forces them to seek shelter. An old couple allow them to stay in their home. The next morning they continue tracking the hoofprints of the bandit troupe.
     <p>They were side tracked by exploring a sea cave. The home of a sea beast and a mysterious door. After becoming cornered by the beast and an encroaching tide, they managed to defeat the creature. Failing to open the door however, they decided to continue down the coast.
@@ -22,6 +27,9 @@ var adventures = [
     <p>Arriving at the home of the squirrel sprites more copper was given, and the squirrels rushed to a well. Upon throwing coins in the well the party found the coins magically disappeared, and the giver was gifted with an enhanced ability.
     <p>Swimming down the well the party found an open (dry) room, with another mysterious door at the end. Four lights on the wall were shown to be eye-like constructs acting as sentries telling the group to stay back from the door. Lokt and Ordrin were able to communicate with the sentries in draconic, receiving translations of the undercommon writings found on the doors and well. They were told this place was the Alms House of Il'manieri, and were given a point on the map of the Temple of Il'manieri. This temple being where to get proper permission to enter the doors, the party decided not to fight the sentries but headed back to town.`)
 ]
+
+loadXMLDoc("/Resources/adventures.xml");
+
 inject('articles');
 
 function inject(into) {
@@ -34,3 +42,15 @@ function inject(into) {
         i++;
     }
 }
+
+function loadXMLDoc(path) {
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        adventureXML = this.responseXML;
+      }
+    };
+    xmlhttp.open("GET", path , true);
+    xmlhttp.send();
+  }
