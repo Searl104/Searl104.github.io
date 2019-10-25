f46c18b747aa470a5f28ac14eac7ad74fa4bea93
Searl104 <56383835+Searl104@users.noreply.github.com>
2019-10-25 14:39:16 -0700

    adverntures

--- adventures.js
+++ adventures.js
@@ -24,7 +24,23 @@ var adventures = [
     <p>Emerald was able to communicate to Chip to lead us to where they take the copper. On the journey Caelynn fell in a large pit, where the party then fought and defeated four giant spiders.
     <p>Arriving at the home of the squirrel sprites more copper was given, and the squirrels rushed to a well. Upon throwing coins in the well the party found the coins magically disappeared, and the giver was gifted with an enhanced ability.
     <p>Swimming down the well the party found an open (dry) room, with another mysterious door at the end. Four lights on the wall were shown to be eye-like constructs acting as sentries telling the group to stay back from the door. Lokt and Ordrin were able to communicate with the sentries in draconic, receiving translations of the undercommon writings found on the doors and well. They were told this place was the Alms House of Il'manieri, and were given a point on the map of the Temple of Il'manieri. This temple being where to get proper permission to enter the doors, the party decided not to fight the sentries but headed back to town.`)
-]
+];
+
+loadXMLDoc();
+function loadXMLDoc() {
+  var xmlhttp;
+  if (window.XMLHttpRequest) {
+    xmlhttp = new XMLHttpRequest();
+  }
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xmlhttp.open("GET", "Resources/adventures.xml", true);
+  xmlhttp.send();
+}
 
 inject('articles');
 function inject(into) {
