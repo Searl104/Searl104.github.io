6d841821eb4bcc118da1deabd20220ca206e5f87
Searl104 <56383835+Searl104@users.noreply.github.com>
2019-11-03 21:44:41 -0800

    Update adventures.js

--- adventures.js
+++ adventures.js
@@ -23,7 +23,15 @@ var adventures = [
     `Caelynn, Lokt, Ordrin, Emerald, Verna, Thalion, and Adara set out from town to begin investigating the copper-thieving squirrels. After spending some time wandering the woods, setting out copper as bait, and only catching glimpses of the fast creatures they finally met one. Thalion killed one of the shapeshifting squirrels (and is now in very bad standing with them) but managed to catch the next one he saw. Emerald then befriended the creature, who she named chip.
     <p>Emerald was able to communicate to Chip to lead us to where they take the copper. On the journey Caelynn fell in a large pit, where the party then fought and defeated four giant spiders.
     <p>Arriving at the home of the squirrel sprites more copper was given, and the squirrels rushed to a well. Upon throwing coins in the well the party found the coins magically disappeared, and the giver was gifted with an enhanced ability.
-    <p>Swimming down the well the party found an open (dry) room, with another mysterious door at the end. Four lights on the wall were shown to be eye-like constructs acting as sentries telling the group to stay back from the door. Lokt and Ordrin were able to communicate with the sentries in draconic, receiving translations of the undercommon writings found on the doors and well. They were told this place was the Alms House of Il'manieri, and were given a point on the map of the Temple of Il'manieri. This temple being where to get proper permission to enter the doors, the party decided not to fight the sentries but headed back to town.`)
+    <p>Swimming down the well the party found an open (dry) room, with another mysterious door at the end. Four lights on the wall were shown to be eye-like constructs acting as sentries telling the group to stay back from the door. Lokt and Ordrin were able to communicate with the sentries in draconic, receiving translations of the undercommon writings found on the doors and well. They were told this place was the Alms House of Il'manieri, and were given a point on the map of the Temple of Il'manieri. This temple being where to get proper permission to enter the doors, the party decided not to fight the sentries but headed back to town.`),
+    new adventure(
+        "Adventure 4: Bullywug Bog",
+     `Caelynn, Lokt, Ordrin, Emerald, Verna, Thalion, and Adara headed southwest out of town to explore that diresction. They ran into an old disheveled woman heading toward town who asked them to look for a specific porous mushroom.
+     <p>They went out through the bog, and met a group of bullywugs. The frog creatures demanded tribute, and each party member handed over something. They in turn received pendant necklaces, seeming to indicate free passage through the bog territory.
+     <p>Finding the coast line the group headed east and found an old tower full of swamp gas. Heading down into the tower they fought some mold creatures, found a lonely construct, and made it to some chests at the bottom. Lokt took the construct back to town to keep in the Order's base house until they can transport it to the well in the northwest.`),
+    new adventure(
+        "Adventure 5: The Wailing Mountains",
+        `In Progress`)
 ];
 
 inject('articles');
